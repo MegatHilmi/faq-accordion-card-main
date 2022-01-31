48f655dd482f6d0f6e37f47237d40a45ea65ed5e
MegatHilmi <megathilmi49@gmail.com>
2022-01-31 22:00:16 +0800

    update text properties when hovering and active

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ let statusArr = new Array(false, false, false, false, false);
 function first(){
     if(firstSur.style.display == "block"){
         firstSur.style.display = "none";
+        document.querySelector(".first-text").style.fontWeight = "400";
         statusArr[0] = false;
         rotatedown(".first-sur-icon");
     }
     else{
         firstSur.style.display = "block";
+        document.querySelector(".first-text").style.fontWeight = "700";
         statusArr[0] = true;
         rotateup(".first-sur-icon");
     }  
@@ -24,11 +26,13 @@ function first(){
 function second(){
     if(secondSur.style.display == "block"){
         secondSur.style.display = "none";
+        document.querySelector(".second-text").style.fontWeight = "400";
         statusArr[1] = false;
         rotatedown(".second-sur-icon");
     }
     else{
         secondSur.style.display = "block";
+        document.querySelector(".second-text").style.fontWeight = "700";
         statusArr[1] = true;
         rotateup(".second-sur-icon");
     }  
@@ -38,11 +42,13 @@ function second(){
 function third(){
     if(thirdSur.style.display == "block"){
         thirdSur.style.display = "none";
+        document.querySelector(".third-text").style.fontWeight = "400";
         statusArr[2] = false;
         rotatedown(".third-sur-icon");
     }
     else{
         thirdSur.style.display = "block";
+        document.querySelector(".third-text").style.fontWeight = "700";
         statusArr[2] = true;
         rotateup(".third-sur-icon");
     }  
@@ -52,11 +58,13 @@ function third(){
 function forth(){
     if(forthSur.style.display == "block"){
         forthSur.style.display = "none";
+        document.querySelector(".forth-text").style.fontWeight = "400";
         statusArr[3] = false;
         rotatedown(".forth-sur-icon");
     }
     else{
         forthSur.style.display = "block";
+        document.querySelector(".forth-text").style.fontWeight = "700";
         statusArr[3] = true;
         rotateup(".forth-sur-icon");
     }  
@@ -66,11 +74,13 @@ function forth(){
 function fifth(){
     if(fifthSur.style.display == "block"){
         fifthSur.style.display = "none";
+        document.querySelector(".fifth-text").style.fontWeight = "400";
         statusArr[4] = false;
         rotatedown(".fifth-sur-icon");
     }
     else{
         fifthSur.style.display = "block";
+        document.querySelector(".fifth-text").style.fontWeight = "700";
         statusArr[4] = true;
         rotateup(".fifth-sur-icon");
     }  
@@ -87,22 +97,27 @@ function check(number){
                 case 0:
                     firstSur.style.display = "none";
                     rotatedown(".first-sur-icon");
+                    document.querySelector(".first-text").style.fontWeight = "400";
                     break;
                 case 1:
                     secondSur.style.display = "none";
                     rotatedown(".second-sur-icon");
+                    document.querySelector(".second-text").style.fontWeight = "400";
                     break;
                 case 2:
                     thirdSur.style.display = "none";
                     rotatedown(".third-sur-icon");
+                    document.querySelector(".third-text").style.fontWeight = "400";
                     break;
                 case 3:
                     forthSur.style.display = "none";
                     rotatedown(".forth-sur-icon");
+                    document.querySelector(".forth-text").style.fontWeight = "400";
                     break;
                 case 4:
                     fifthSur.style.display = "none";
                     rotatedown(".fifth-sur-icon");
+                    document.querySelector(".fifth-text").style.fontWeight = "400";
                     break;                                    
             }
         }
